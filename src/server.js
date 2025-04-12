3fa40ff016eee41aeb8a903fad1f418971c92b7b
leninfabricio29 <leninfabricio7@gmail.com>
2025-04-11 22:48:02 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT ; // Establece el puerto para el servidor
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`API activa en el puerto${PORT}`);
+  console.log(`Servidor en el puerto${PORT}`);
 });
