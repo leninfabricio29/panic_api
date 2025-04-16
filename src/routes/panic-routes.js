b46a36f913369f50a9a26c4b3856f03a8bac0b83
leninfabricio29 <leninfabricio7@gmail.com>
2025-04-16 00:33:12 -0500

    Update panic-routes.js

--- src/routes/panic-routes.js
+++ src/routes/panic-routes.js
@@ -1,11 +1,11 @@
-// routes/panic.js
-const express = require('express');
-const router = express.Router();
-const { handlePanic } = require('../controllers/panic-controller');
-const { protect } = require('../middlewares/auth');
+    // routes/panic.js
+    const express = require('express');
+    const router = express.Router();
+    const { handlePanic } = require('../controllers/panic-controller');
+    const { protect } = require('../middlewares/auth');
 
-router.use(protect)
+    router.use(protect)
 
-router.post('/alerta', handlePanic);
+    router.post('/alerta', handlePanic);
 
-module.exports = router;
+    module.exports = router;
