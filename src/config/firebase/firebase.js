28b12b7618893be19daeee1b104997c9455220c2
leninfabricio29 <leninfabricio7@gmail.com>
2025-04-18 23:44:26 -0500

    Configuracion Lista

--- src/config/firebase/firebase.js
+++ src/config/firebase/firebase.js
@@ -1,8 +1,10 @@
-// src/config/firebase/firebase.js
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+const decodedBase64 = Buffer.from(process.env.FIREBASE_CREDENTIALS_BASE64, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(decodedBase64);
+
+// Firebase usa \n para las claves privadas, así que lo restauramos correctamente:
 serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
